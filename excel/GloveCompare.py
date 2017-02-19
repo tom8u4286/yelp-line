5110c515fb3d54da89e460cdb50fed47efaa9395
Tomtom <tom8u4286@gmail.com>
2017-02-19 21:44:30 +0800

    excel/

--- excel/GloveCompare.py
+++ excel/GloveCompare.py
@@ -10,10 +10,10 @@ offset = 0
 for r in range(1,500):
     rest_num = r
     try:
-        f1 = open("data/rank/restaurant_%s_rank_type1.json"%rest_num )
-        f2 = open("data/rank/restaurant_%s_rank_type2.json"%rest_num )
-        f3 = open("data/rank/restaurant_%s_rank_type3.json"%rest_num )
-        f4 = open("data/rank/restaurant_%s_rank_type4.json"%rest_num )
+        f1 = open("../data/rank/restaurant_%s_rank_type1.json"%rest_num )
+        f2 = open("../data/rank/restaurant_%s_rank_type2.json"%rest_num )
+        f3 = open("../data/rank/restaurant_%s_rank_type3.json"%rest_num )
+        f4 = open("../data/rank/restaurant_%s_rank_type4.json"%rest_num )
     except:
         print "cannot open file: rest%s"%(r)
         continue
