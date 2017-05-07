8da0da1f494bd7eaee0a91ee438a57e38f746b40
Yi-Fan Chu <yfchu@clip4.cs.nccu.edu.tw>
2017-05-07 14:19:36 +0800

    RestClean.py: remove the rest which are not from LV (remainging 265 rests)

--- RestClean.py
+++ RestClean.py
@@ -4,9 +4,11 @@ from collections import OrderedDict
 
 business_list = json.load(open("data/business_list.json"))
 new_business_list = []
-remove_list = [2, 46, 49, 377, 491, 446, 484]
+#remove_list = [2, 46, 49, 118, 377, 392, 491, 446, 484]
+
 for rest in business_list:
-    if rest["index"] in remove_list:
+    #if rest["index"] in remove_list or rest['city'] != 'Las Vegas':
+    if rest['city'] != 'Las Vegas':
         continue
     else:
         new_business_list.append(rest)
