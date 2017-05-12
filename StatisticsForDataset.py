38f5d0dc9645a7b702890c101e49b0f46ce551ed
Yi-Fan Chu <yfchu@clip4.cs.nccu.edu.tw>
2017-05-12 20:18:05 +0800

    StatisticsForDataset.py 264rests

--- StatisticsForDataset.py
+++ StatisticsForDataset.py
@@ -6,7 +6,7 @@ avg_senti_per_review = 0
 dish_senti_avg_length = 0
 
 
-for i in range(1,266):
+for i in range(1,265):
     if i == 206:
         continue
     f_dic = open('data/restaurant_dict_list/restaurant_dict_%s.json'%i)
@@ -18,7 +18,7 @@ for i in range(1,266):
     dish_senti_avg_length += rest_dic['dish_senti_avg_len']
 
 print 'total reviews count: ',total_reviews_count
-print 'avg_word_count_pre_review: ',float(avg_word_count_pre_review)/265
-print 'avg_menu_length: ',float(avg_menu_length)/265
-print 'avg_senti_per_review: ',float(avg_senti_per_review)/265
-print 'dish_senti_avg_length: ',float(dish_senti_avg_length)/265
+print 'avg_word_count_pre_review: ',float(avg_word_count_pre_review)/264
+print 'avg_menu_length: ',float(avg_menu_length)/264
+print 'avg_senti_per_review: ',float(avg_senti_per_review)/264
+print 'dish_senti_avg_length: ',float(dish_senti_avg_length)/264
