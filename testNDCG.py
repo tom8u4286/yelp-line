b540ca027a4fcde3533429ec96ff27cff9b8f497
Yi-Fan Chu <yfchu@clip4.cs.nccu.edu.tw>
2017-06-05 12:07:13 +0800

    testNDCG.py

--- testNDCG.py
+++ testNDCG.py
@@ -1,38 +1,119 @@
 import json
+import sys
+import numpy as np
 from operator import itemgetter
-f = open('restaurant_1_rank_type3.json')
 
+
+def dcg_at_k(r, k, method=0):
+    """Score is discounted cumulative gain (dcg)
+    Relevance is positive real values.  Can use binary
+    as the previous methods.
+    Example from
+    http://www.stanford.edu/class/cs276/handouts/EvaluationNew-handout-6-per.pdf
+    >>> r = [3, 2, 3, 0, 0, 1, 2, 2, 3, 0]
+    >>> dcg_at_k(r, 1)
+    3.0
+    >>> dcg_at_k(r, 1, method=1)
+    3.0
+    >>> dcg_at_k(r, 2)
+    5.0
+    >>> dcg_at_k(r, 2, method=1)
+    4.2618595071429155
+    >>> dcg_at_k(r, 10)
+    9.6051177391888114
+    >>> dcg_at_k(r, 11)
+    9.6051177391888114
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+        k: Number of results to consider
+        method: If 0 then weights are [1.0, 1.0, 0.6309, 0.5, 0.4307, ...]
+                If 1 then weights are [1.0, 0.6309, 0.5, 0.4307, ...]
+    Returns:
+        Discounted cumulative gain
+    """
+    r = np.asfarray(r)[:k]
+    if r.size:
+        if method == 0:
+            return r[0] + np.sum(r[1:] / np.log2(np.arange(2, r.size + 1)))
+        elif method == 1:
+            return np.sum(r / np.log2(np.arange(2, r.size + 2)))
+        else:
+            raise ValueError('method must be 0 or 1.')
+    return 0.
+
+
+def ndcg_at_k(r, k, method=0):
+    """Score is normalized discounted cumulative gain (ndcg)
+    Relevance is positive real values.  Can use binary
+    as the previous methods.
+    Example from
+    http://www.stanford.edu/class/cs276/handouts/EvaluationNew-handout-6-per.pdf
+    >>> r = [3, 2, 3, 0, 0, 1, 2, 2, 3, 0]
+    >>> ndcg_at_k(r, 1)
+    1.0
+    >>> r = [2, 1, 2, 0]
+    >>> ndcg_at_k(r, 4)
+    0.9203032077642922
+    >>> ndcg_at_k(r, 4, method=1)
+    0.96519546960144276
+    >>> ndcg_at_k([0], 1)
+    0.0
+    >>> ndcg_at_k([1], 2)
+    1.0
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+        k: Number of results to consider
+        method: If 0 then weights are [1.0, 1.0, 0.6309, 0.5, 0.4307, ...]
+                If 1 then weights are [1.0, 0.6309, 0.5, 0.4307, ...]
+    Returns:
+        Normalized discounted cumulative gain
+    """
+    dcg_max = dcg_at_k(sorted(r, reverse=True), k, method)
+    if not dcg_max:
+        return 0.
+    return dcg_at_k(r, k, method) / dcg_max
+
+
+ground_truth = json.load(open('data/GroundTruth/rest1.json'))
+rest_dic = json.load(open('data/restaurant_dict_list/restaurant_dict_1.json'))
+dishes = rest_dic['menu']
+ground_truth_ar = []
+for dish in ground_truth:
+    for d in dishes:
+        if dish[1] == d['name']:
+            ground_truth_ar.append(d['name_ar'])
+
+f = open('data/rank/restaurant_1_rank_type3.json')
 rank_dic = json.load(f)
 dish_list = rank_dic['rank']
 
-top_list = []
-for num in range(1,6):
+method_list = []
+for d in ground_truth_ar:
     for dish in dish_list:
-        if dish['rank_by_frq'] == num or dish['rank_by_sum_higher0_cos_zXnorm_frq'] == num:
-            top_list.append(dish['dish'])
+        if dish['dish'] == d:
+            method_list.append([dish['rank_by_sum_higher0_cos_zXnorm_frq'],dish['dish']])
 
-lst = []
-for dish in dish_list:
-    for item in top_list:
-        if dish['dish'] == item:
-            dic = {}
-            dic['dish'] = dish['dish']
-            dic['rank_by_sum_higher0_cos_zXnorm_frq'] = dish['rank_by_sum_higher0_cos_zXnorm_frq']
-            lst.append(dic)
-lst = sorted(lst, key=itemgetter('rank_by_sum_higher0_cos_zXnorm_frq'))
-for i in lst:
-    print i
+method_list = sorted(method_list, key=itemgetter(0))
 
+print 'method:'
+for d in method_list:
+    print d
 
-ground_truth = ['baguette_mon-ami-gabi','grilled-pork-tenderloin_mon-ami-gabi','salmon-spinach-and-baby-kale-salad_mon-ami-gabi','chicken-grand-mere_mon-ami-gabi','ratatouille_mon-ami-gabi','salmon_mon-ami-gabi','steak-frites_mon-ami-gabi','mac-cheese_mon-ami-gabi','french-onion-soup_mon-ami-gabi','frites_mon-ami-gabi']
+print 'gt:'
+for d in ground_truth_ar:
+    print d
 
-ndcg = [4,4,3,3,2,2,1,1,0,0]
-count = 0
-for d, o, score in zip(ground_truth, lst, ndcg):
-    #print d , o['dish']
-    if d == o['dish']:
-        count += score
-print 'ndcg: ', float(count)/ float(sum(ndcg))
+i = [4,4,3,3,2,2,1,1,0,0]
+r = []
+for dish in method_list:
+    for num, d in zip(i, ground_truth_ar):
+        if dish[1] == d:
+            r.append(num)
+print 'r:', r
+
+print 'ndcg@10:',ndcg_at_k(r, 10, method=0)
 
 
 
